18c7e3726d1578f044bae9fe67ea0f31d23b68fc
daniyal <114733266+d4niy4l@users.noreply.github.com>
2024-05-27 21:40:48 +0500

    .

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://bloggo-five.vercel.app/");
+    res.setHeader("Access-Control-Allow-Origin", "https://bloggo-five.vercel.app");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials
