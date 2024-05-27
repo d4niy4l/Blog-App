b905f44d9b36a055537f8707b809f3539a7042b2
daniyal <114733266+d4niy4l@users.noreply.github.com>
2024-05-27 22:32:04 +0500

    cors issue pt 2

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,11 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://bloggo-five.vercel.app");
+    const allowedOrigins = ["https://bloggo-five.vercel.app"];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials
