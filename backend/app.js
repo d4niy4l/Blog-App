14d3db93591d76aa9c6c991c659cfe189be1e785
daniyal <114733266+d4niy4l@users.noreply.github.com>
2024-05-27 22:37:01 +0500

    push

--- backend/app.js
+++ backend/app.js
@@ -18,16 +18,22 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
+app.use(cors({
+    origin: 'https://bloggo-five.vercel.app',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
+  
 app.use((req, res, next) => {
     const allowedOrigins = ["https://bloggo-five.vercel.app"];
     const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
-    }
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
     next();
 });
 app.use(cookie_parser());
